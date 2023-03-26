790691498a563a26d7c777695e66df68b5617365
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-03-27 02:26:00 +0300

    Реализовал основыне настройки вебпака

---
+++ dist/main.85c149e098c40ff2cac8.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";const t=document.querySelector("#app");[{id:1,title:"Item 1"},{id:2,title:"Item 2"},{id:3,title:"Item 3"},{id:4,title:"Item 4"},{id:5,title:"Item 5"}].forEach((function(e){const i=document.createElement("li");i.textContent=e.title,t.append(i)}))})();
+//# sourceMappingURL=main.85c149e098c40ff2cac8.js.map
