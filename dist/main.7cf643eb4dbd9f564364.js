c41806ceffc2d108735caa05211895b3ae2c3449
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-03-27 23:37:55 +0300

    Реализовал основные стили + изменил структуру html

---
+++ dist/main.7cf643eb4dbd9f564364.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("index.js")})();
+//# sourceMappingURL=main.7cf643eb4dbd9f564364.js.map
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import data from './data';
 import './index.scss';
 
-const root = document.querySelector('#app')
-
-function renderItem(item) {
-  const li = document.createElement('li')
-  li.textContent = item.title
-  root.append(li)
-}
-
-data.forEach(renderItem)
+console.log('index.js');
