e3c70e7cf7c6bcbe2a2a7e64d33d0495680e4888
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-03-28 02:11:27 +0300

    Вынес  массив в data.js

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    iconPath: "images/sun.svg",
+    iconAlt: 'sun icon',
+    backgroundPath: 'url(images/summer-bg.jpg)'
+  },
+  {
+    iconPath: "images/cloud-rain.svg",
+    iconAlt: 'rain icon',
+    backgroundPath: 'url(images/rainy-bg.jpg)'
+  },
+  {
+    iconPath: "images/cloud-snow.svg",
+    iconAlt: 'snow icon',
+    backgroundPath: 'url(images/winter-bg.jpg)'
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
+import cardsInfo from './data';
 import './index.scss';
-
-const cardsInfo = [
-  {
-    iconPath: "images/sun.svg",
-    iconAlt: 'sun icon',
-    backgroundPath: 'url(images/summer-bg.jpg)'
-  },
-  {
-    iconPath: "images/cloud-rain.svg",
-    iconAlt: 'rain icon',
-    backgroundPath: 'url(images/rainy-bg.jpg)'
-  },
-  {
-    iconPath: "images/cloud-snow.svg",
-    iconAlt: 'snow icon',
-    backgroundPath: 'url(images/winter-bg.jpg)'
-  }
-];
 
 const audioCardTemplate = document.querySelector('.template-audio-card').content.querySelector('.audio-cards__container');
 const listAudioCards = document.querySelector('.audio-cards');
