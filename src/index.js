4b48036f0642eee457ddb387c13a1aa60c239e1e
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-03-29 03:26:49 +0300

    Реализовал корректную визуализацию паузы

--- src/index.js
+++ src/index.js
@@ -12,23 +12,37 @@ function createAudioCard(item) {
   const audioCard = cardElement.querySelector('.audio-cards__card');
   const cardIcon = cardElement.querySelector('.audio-cards__icon');
   const audio = cardElement.querySelector('.audio-cards__audio');
+  const pause = 'images/pause.svg';
 
   audio.src = item.audioPath;
   audio.id = item.id;
   cardIcon.src = item.iconPath;
   cardIcon.setAttribute('alt', item.iconAlt);
   audioCard.style.backgroundImage = item.backgroundPath;
-  console.log('audio: ', audio.loop);
 
   function playAudio() {
     if (switchingFlag === 0) {
       audio.play();
+      cardIcon.src = pause;
       switchingFlag = item.id;
     } else if (switchingFlag === item.id) {
-      !audio.paused ? audio.pause() : audio.play();
+      if (!audio.paused) {
+        audio.pause();
+        cardIcon.src = item.iconPath;
+      } else {
+        audio.play();
+        cardIcon.src = pause;
+      }
       switchingFlag = item.id;
     } else {
       const currentAudio = document.getElementById(`${switchingFlag}`);
+      document.querySelectorAll('.audio-cards__icon')
+        .forEach((i, idx) => {
+          if (switchingFlag - 1 === idx) {
+            i.src = cardsInfo[idx].iconPath;
+          }
+        });
+      cardIcon.src = pause;
       currentAudio.pause();
       audio.play();
       switchingFlag = item.id;
