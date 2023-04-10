0c0dd3606b8a7573a863fb920a9c20b47f975984
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-04-10 05:06:02 +0300

    Поправил typescript, реализовал функцию totalPrice

---
+++ typescript-functions/src/priceFunction.ts
@@ -0,0 +1,26 @@
+interface TotalPriceParam {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  months: number;
+}
+
+const totalPrice = ({ price, discount, isInstallment, months }: TotalPriceParam): number => {
+  let finalPrice: number = price;
+
+  if (price && price > 0) {
+    if (months && months > 0 && isInstallment) {
+      finalPrice = (price - (price / 100 * discount)) / months;
+    } else if (months && months > 0 && !isInstallment) {
+      finalPrice = price - (price / 100 * discount);
+    } else {
+      console.log('Количество месяцев должно быть не меньше 0');
+    }
+  } else {
+    console.log('Цена должна быть больше 0');
+  }
+
+  return finalPrice;
+};
+
+console.log(totalPrice({ price: 458, discount: 50, isInstallment: true, months: 30 }));
