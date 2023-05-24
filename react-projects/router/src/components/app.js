0ae875d5ded3ae82a698240b25c052228a629958
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-05-24 04:44:14 +0300

    Реализовал всю  логику связанную с роутингом

--- react-projects/router/src/components/app.js
+++ react-projects/router/src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link, useRoutes, NavLink, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavigationPanel from "./navigationPanel";
 import BaseList from "./baseList";
 
@@ -9,10 +9,10 @@ function App() {
       <NavigationPanel />
 
       <Routes>
-        <Route path="/" element={<BaseList />}></Route>
-        <Route path="/characters" element={<BaseList />}></Route>
-        <Route path="/episode" element={<BaseList />}></Route>
-        <Route path="/location" element={<BaseList />}></Route>
+        <Route path="/" element={<BaseList page="home" key={4} />} />
+        <Route path="/characters/*" element={<BaseList page="characters" key={1} />} />
+        <Route path="/episode/*" element={<BaseList page="episode" key={2} />} />
+        <Route path="/location/*" element={<BaseList page="location" key={3} />} />
       </Routes>
     </div>
   );
--- react-projects/router/src/components/baseList.js
+++ react-projects/router/src/components/baseList.js
@@ -1,10 +1,83 @@
 import React from "react";
+import InfoCard from "./infoCard";
+import { charactersData } from "../data/charactersData";
+import { episodeData } from "../data/episodeData";
+import { locationData } from "../data/locationData";
+import { NavLink, Routes, Route } from "react-router-dom";
 
-{/* <h2>Добро пожаловать во вселенную Рика и Морти!</h2> */ }
+const BaseList = ({ page }) => {
 
-const BaseList = () => {
+  const list = React.useMemo(() => {
+    if (page === "home") {
+      return (
+        <h2>Добро пожаловать во вселенную Рика и Морти!</h2>
+      );
+    } else if (page === "characters") {
+      const charactersList = charactersData.map((item) => {
+        return (
+          <NavLink to={`/characters/${item.id}`} key={item.name}>{item.name}</NavLink>
+        )
+      });
+      return (
+        <>{charactersList}</>
+      )
+    } else if (page === "episode") {
+      const episodeList = episodeData.map((item) => {
+        return (
+          <NavLink to={`/episode/${item.id}`} key={item.name}>{item.name}</NavLink>
+        )
+      });
+      return (
+        <>{episodeList}</>
+      )
+    } else if (page === "location") {
+      const locationList = locationData.map((item) => {
+        return (
+          <NavLink to={`/location/${item.id}`} key={item.name}>{item.name}</NavLink>
+        )
+      });
+      return (
+        <>{locationList}</>
+      )
+    }
+  }, [page]);
+
+  const routes = React.useMemo(() => {
+    if (page === "characters") {
+      const charactersRoutes = charactersData.map((item, idx) => {
+        return (
+          <Route key={item.id} path={`${item.id}`} element={<InfoCard id={item.id} />} />
+        )
+      });
+      return (
+        <Routes>{charactersRoutes}</Routes>
+      )
+    } else if (page === "episode") {
+      const episodeRoutes = episodeData.map((item, idx) => {
+        return (
+          <Route key={item.id} path={`${item.id}`} element={<InfoCard id={item.id} />} />
+        )
+      });
+      return (
+        <Routes>{episodeRoutes}</Routes>
+      )
+    } else if (page === "location") {
+      const locationRoutes = locationData.map((item, idx) => {
+        return (
+          <Route key={item.id} path={`${item.id}`} element={<InfoCard id={item.id} />} />
+        )
+      });
+      return (
+        <Routes>{locationRoutes}</Routes>
+      )
+    }
+  }, [page]);
+
   return (
-    <div>HELLO!!!</div>
+    <div key={page}>
+      {list}
+      {routes}
+    </div>
   )
 };
 
